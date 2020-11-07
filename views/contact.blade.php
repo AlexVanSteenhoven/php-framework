c40d2323d9436447c0823cb3da9e0ab5a4e706ac
Alex van Steenhoven <alex.steenhoven@gmail.com>
2020-11-07 17:04:19 +0100

    Created bootstrap contact form

--- views/contact.blade.php
+++ views/contact.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
     <div class="container">
-        <h1 class="text-center">Contact</h1>
+        <h1 class="text-center">Contact form</h1>
+        <div class="py-5">
+            <div class="card">
+                <div class="card-body">
+                    <form action="" method="POST">
+                        <div class="form-group">
+                          <label for="email">Email address</label>
+                          <input type="email" class="form-control" id="email" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                          <label for="subject">Password</label>
+                          <input type="email" class="form-control" id="subject" placeholder="Subject">
+                        </div>
+                        <div class="form-group">
+                          <label for="message">Message</label>
+                          <textarea class="form-control" name="message" id="message"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
