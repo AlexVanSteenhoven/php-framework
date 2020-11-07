2b658e5f17e9c224c9e3308ee5281dd45c02f9fa
Alex van Steenhoven <alex.steenhoven@gmail.com>
2020-11-07 17:24:10 +0100

    added routes in index.php

--- public/index.php
+++ public/index.php
@@ -2,14 +2,20 @@
 
 /** @author: Alex van Steenhoven <alex.steenhoven@gmail.com> */
 
+use app\controllers\AuthController;
 use app\core\Application;
+use app\controllers\HomeController;
+use app\controllers\ContactController;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application();
 
-$app->router->get('/', 'home');
-
-$app->router->get('/contact', 'contact');
+// Routes
+$app->router->get('/', [HomeController::class, 'viewHome']);
+$app->router->get('/contact', [ContactController::class, 'viewContact']);
+$app->router->post('/contact', [ContactController::class, 'handleContact']);
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
 
 $app->run();
